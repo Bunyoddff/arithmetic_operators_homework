8cca98c6371501667eee27c15b1a9ce75e18f60f
Bunyoddff <bkhusanov444@gmail.com>
2022-12-22 16:06:06 +0500

    py006

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -3,7 +3,7 @@ number=33
 #Divide the number by 2 and assign number.
 number=number/2
 #Find the fraction of the number and assign to a variable called 'answer'.
-answer=(number-int(number))*10
+answer=(number%1)
 #Print the answer.
 print(answer)
 #Print the number.
